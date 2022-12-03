a21369b5d1ecc0ad05dbd8c46bc8c403e8173d04
Cisco-coco <jiachengxu@mail.ustc.edu.cn>
2022-12-03 12:19:49 +0800

    modify get_memory function

--- watch_memory.py
+++ watch_memory.py
@@ -5,4 +5,5 @@ def get_memory():
     pid = os.getpid()
     p = psutil.Process(pid)
     info = p.memory_full_info()
+    print("Memory usage: {:.2f} MB".format(info.uss / 1024. / 1024.))
     return info.uss / 1024. / 1024. # in MB
