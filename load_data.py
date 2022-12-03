3efc82cf3675ee1aa52a9292a34162523693db97
PhoebeXL <2669144437@qq.com>
2022-12-03 10:45:36 +0800

    count

--- load_data.py
+++ load_data.py
@@ -3,4 +3,13 @@ import json
 path = "train_data_all.json"
 with open(path, 'r') as f:
     dataset = json.load(f)
-    print(dataset)
+    # print(dataset)
+# print(len(dataset))
+
+count = 0
+for val in dataset:
+    if val['weight'] != '' and val['height'] != '' and val['fit'] != '' and val['size'] != '' and val['size'] != 'NONE' and val['usually_wear'] != '':
+        count += 1
+
+print(count)
+
