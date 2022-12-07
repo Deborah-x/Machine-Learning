5eaaf171302b6f0aa6b0beddfcb60805c2e56972
PhoebeXL <2669144437@qq.com>
2022-12-07 13:09:00 +0800

    统计了一下不同关键词的可选范围

--- data_analysis.py
+++ data_analysis.py
@@ -4,13 +4,17 @@ import json
 import numpy as np
 import pandas as pd
 
+
 def load_dataset():
+    # 加载训练数据集
     path = "train_data_all.json"
     with open(path, 'r') as f:
         dataset = json.load(f)
         return dataset
 
+
 def key_num(data, *args):
+    # 统计所给关键词都不为空的训练数据数量
     print(args)
     count = 0
     for index in data:
@@ -22,14 +26,15 @@ def key_num(data, *args):
         count += flag
     return count
 
-# print(key_num('fit', 'weight', 'height', 'size', 'usually_wear'))
 
 def drop_nan():
+    # 将训练数据集中有空信息的数据扔掉，剩余数据放入一个新文件
     data_raw = load_dataset()
     pd.DataFrame(data_raw).to_csv('data_proc.txt', index=False)
     pd.read_csv('data_proc.txt').dropna().to_csv('data_proc.txt', index=False)
-    # df = pd.read_csv('data_proc.txt')
-    # return df
+    df = pd.read_csv('data_proc.txt')
+    return df
+
 
 def convert_h():
     # 将身高从英尺转化为标准单位厘米, 保留两位小数
@@ -41,4 +46,12 @@ def convert_h():
     df.to_csv('data_proc.txt', index=False)
     print(df['height'])
 
-convert_h()
+
+def val_range(key):
+    # 统计关键词的范围
+    df = pd.DataFrame(load_dataset())
+    return set(df[key])
+
+# convert_h()
+# df = pd.read_csv('data_proc.txt')
+print(val_range('rating'))
