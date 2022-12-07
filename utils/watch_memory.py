440df3bdada5d7fbf3f21e7547e93db09bfdfb45
Cisco-coco <jiachengxu@mail.ustc.edu.cn>
2022-12-07 13:45:26 +0800

    add para verbose and move it to utils/

--- utils/watch_memory.py
+++ utils/watch_memory.py
@@ -1,9 +1,10 @@
 import psutil
 import os
 
-def get_memory():
+def get_memory(verbose=False):
     pid = os.getpid()
     p = psutil.Process(pid)
     info = p.memory_full_info()
-    print("Memory usage: {:.2f} MB".format(info.uss / 1024. / 1024.))
+    if verbose:
+        print("Memory usage: {:.2f} MB".format(info.uss / 1024. / 1024.))
     return info.uss / 1024. / 1024. # in MB
