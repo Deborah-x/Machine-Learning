71e14527f3302c9d1cedb5e92b85e607338e7de0
PhoebeXL <2669144437@qq.com>
2022-12-11 12:16:58 +0800

    对数据进行了一些处理

--- data_analysis.py
+++ data_analysis.py
@@ -1,4 +1,5 @@
 import os
+import re
 import csv
 import json
 import numpy as np
@@ -6,52 +7,83 @@ import pandas as pd
 
 
 def load_dataset():
-    # 加载训练数据集
+    # 加载训练数据集train_data_all.json，返回值为list类型
     path = "train_data_all.json"
     with open(path, 'r') as f:
         dataset = json.load(f)
         return dataset
 
+def build_data_raw():
+    # 将数据集转化为dataframe类型并放入data_raw.txt
+    pd.DataFrame(load_dataset()).to_csv('data_raw.txt', index=False)
 
-def key_num(data, *args):
-    # 统计所给关键词都不为空的训练数据数量
-    print(args)
-    count = 0
-    for index in data:
-        flag = 1
-        for key in args:
-            if index[key] == '':
-                flag = 0
-                break
-        count += flag
-    return count
+# def key_num(data, *args):
+#     # 统计所给关键词都不为空的训练数据数量
+#     print(args)
+#     count = 0
+#     for index in data:
+#         flag = 1
+#         for key in args:
+#             if index[key] == '':
+#                 flag = 0
+#                 break
+#         count += flag
+#     return count
 
-
-def drop_nan():
+def drop_nan(spath, dpath):
     # 将训练数据集中有空信息的数据扔掉，剩余数据放入一个新文件
-    data_raw = load_dataset()
-    pd.DataFrame(data_raw).to_csv('data_proc.txt', index=False)
-    pd.read_csv('data_proc.txt').dropna().to_csv('data_proc.txt', index=False)
-    df = pd.read_csv('data_proc.txt')
-    return df
+    data = pd.read_csv(spath)
+    data.dropna().to_csv(dpath, index=False)
 
-
-def convert_h():
+def convert_h(path):
     # 将身高从英尺转化为标准单位厘米, 保留两位小数
     # 1ft = 30.48cm
     # 1in =  2.54cm
-    df = drop_nan()
-    df = pd.read_csv('data_proc.txt')
-    df['height'] = df['height'].map(lambda x: round(int(x[0]) * 30.48 + int(x[3]) * 2.54, 2))
-    df.to_csv('data_proc.txt', index=False)
-    print(df['height'])
+    df = pd.read_csv(path)
+    df['height'] = df['height'].map(lambda x: round(int(x[0]) * 30.48 + int(x[3]) * 2.54, 2) if re.match(r'\d\' \d\"', str(x)) else 162) # 162大概是身高的平均水平
+    df.to_csv(path, index=False)
+    # print(df['height'])
 
+def convert_w(path):
+    df = pd.read_csv(path)
+    df['weight'] = df['weight'].map(lambda x: int(x.strip('LBS')) if re.match(r'\d{2,3}LBS', x) else 140) # 140是体重的平均水平
+    df.to_csv(path, index=False)
+    # print(df['weight'])
 
-def val_range(key):
-    # 统计关键词的范围
-    df = pd.DataFrame(load_dataset())
+def convert_f(path):
+    data = pd.read_csv(path)
+    data['fit'] = data['fit'].replace(['Small', 'True to Size', 'Large'], [-1, 0, 1])
+    data.to_csv(path, index=False)
+
+def convert_n(path):
+    data = pd.read_csv(path)
+    uni = data['item_name'].unique()
+    map = dict(zip(uni, list(range(len(uni)))))
+    # print(map)
+    data['item_name'] = data['item_name'].replace(map)
+    data.to_csv(path, index=False)
+
+def val_range(path, key):
+    # 统计数据集中关键词的范围
+    df = pd.read_csv(path)
     return set(df[key])
 
-# convert_h()
-# df = pd.read_csv('data_proc.txt')
-print(val_range('rating'))
+def proc():
+    spath = 'data_raw.txt'
+    dpath = 'data_proc.txt'
+    drop_nan(spath, dpath)
+    convert_h(dpath)
+    convert_w(dpath)
+    convert_f(dpath)
+    convert_n(dpath)
+
+def k_means():
+    pass
+
+spath = 'data_raw.txt'
+dpath = 'data_proc.txt'
+# proc()
+# print(val_range(dpath, 'rating'))
+# print(val_range(dpath, 'fit'))
+data = pd.read_csv(dpath)
+print(data[['item_name','fit','height','weight','rating']])
