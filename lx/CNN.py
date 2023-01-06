60a13c5d7ba2eb50b3d64f8da7a4f5b1c09ac685
Deborah-x <2669144437@qq.com>
2023-01-06 19:51:00 +0800

    使用embedding处理成词向量准确率还是只有三十多

--- lx/CNN.py
+++ lx/CNN.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn
 import numpy as np
 from tqdm import tqdm
 from torchvision import transforms
@@ -8,14 +9,23 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 labels = np.load('label_mix.npy')
-dataSet = np.load('data_mix.npy')
-m, n = dataSet.shape
+# dataSet = np.load('data_mix.npy')
+# m, n = dataSet.shape
 
 # print(dataSet.shape)  # (22071,4981),数据均衡后(45000,4981)
-pad = np.zeros((m, 1419))
-dataSet = np.concatenate((dataSet, pad), axis=1).reshape((m,80,80))
+# pad = np.zeros((m, 1419))
+# dataSet = np.concatenate((dataSet, pad), axis=1).reshape((m,80,80))
 # print(dataSet.shape)  # (22071,6400),数据均衡后(45000,6400)
 # print(dataSet.shape)  # (22071,80,80),数据均衡后(45000,80,80)
+
+# 尝试用embedding处理数据
+embedding = torch.nn.Embedding(5000, 1000)
+data = np.load('data2embedding.npy')
+m, n = data.shape
+dataSet = embedding(torch.LongTensor(data))
+dataSet = torch.reshape(dataSet, (22071,100,100))
+# print(dataSet.shape)
+#
 labels = torch.tensor(labels, dtype=torch.long)
 dataSet = torch.tensor(dataSet, dtype=torch.float)
 labels = labels-1
@@ -25,12 +35,12 @@ labels = labels-1
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = torch.nn.Conv2d(1,5,kernel_size=5, padding=2)
-        self.conv2 = torch.nn.Conv2d(5,10,kernel_size=5, padding=2)
-        self.conv3 = torch.nn.Conv2d(10,5,kernel_size=5, padding=2)
-        self.conv4 = torch.nn.Conv2d(5,5,kernel_size=5, padding=2)
+        self.conv1 = torch.nn.Conv2d(1,2,kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(2,3,kernel_size=3, padding=1)
+        self.conv3 = torch.nn.Conv2d(3,4,kernel_size=3, padding=1)
+        self.conv4 = torch.nn.Conv2d(4,5,kernel_size=3, padding=1)
         self.pooling = torch.nn.MaxPool2d(2)
-        self.fc = torch.nn.Linear(125,3)   # fc:full connected layer 全连接层
+        self.fc = torch.nn.Linear(180,3)   # fc:full connected layer 全连接层
 
     def forward(self, x):
         # Flatten data from (n,1,28,28) to (n,784)
@@ -53,7 +63,6 @@ optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.5)
 def train(epoch):
     for i in range(epoch):
         for j in tqdm(range(m)):
-            # 获得一个批次的数据和标签
             inputs = dataSet[j:j+1]
             target = labels[j:j+1]
             optimizer.zero_grad()
@@ -79,7 +88,7 @@ def test(epoch):
  
  
 if __name__ == '__main__':
-    train(2)
+    train(10)
     test(m)
     # print(model)
     pass
