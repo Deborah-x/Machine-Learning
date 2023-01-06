ed31492c1028eefe497d094a97581eaaa28c5f97
YiboChen2020 <already_yet@mail.ustc.edu.cn>
2023-01-07 00:09:19 +0800

    bayes for 5 predictor

--- cyb/data_analysis.py
+++ cyb/data_analysis.py
@@ -4,6 +4,7 @@ import csv
 import json
 import numpy as np
 import pandas as pd
+import tqdm
 
 
 def load_dataset():
@@ -30,16 +31,7 @@ def build_data_raw():
 #         count += flag
 #     return count
 
-def drop_nan(spath, dpath):
-    # 将训练数据集中有空信息的数据扔掉，剩余数据放入一个新文件
-    data = pd.read_csv(spath, usecols = ['review_summary','review','fit','rating'])
-    df = data.dropna()
-    m, n = df.shape
-    for i in range(m):
-        for j in range(n):
-            x = df.iat[i,j]
-            df.iat[i,j] = str(x).replace('\n',' ')
-    df.to_csv(dpath, index=False)
+
 
 def get_review(spath, dpath):
     data = pd.read_csv(spath)[['review','fit']]
@@ -100,13 +92,6 @@ def convert_r(path):
     # data['rating'] = data['rating'].replace([0, 1, 2, 3, 4, 5], [0, 10, 20, 30, 40, 50])
     data.to_csv(path, index=False)
 
-def val_range(path, key):
-    # 统计数据集中关键词的范围
-    df = pd.read_csv(path)
-    return set(df[key])
-
-
-
 def proc():
     spath = 'data_raw.txt'
     dpath = 'data_proc.txt'
@@ -121,14 +106,96 @@ def proc():
     # convert_h(dpath)
     # convert_w(dpath)
 
+def val_range(path, key):
+    '''
+    统计数据集中关键词的范围，返回值为set类型
+    '''
+    df = pd.read_csv(path)
+    # print(df.shape)
+    return set(df[key])
 
-build_data_raw()
+def delete_len1(list):
+   for i in range(len(list) - 1, -1, -1):
+      if len(list[i]) == 1 and not list[i].isdigit():
+         del list[i]
+
+def split2word(words):
+   word = []
+   for i in range(len(words)):
+      word += words[i].split()
+   delete_len1(word)
+   return word
+
+def build_list():
+    f = open(r"C:/Users/86189/PycharmProjects/MLproject/Machine-Learning/cyb/data_proc.txt", 'r', encoding="utf-8")
+    set = f.readlines()
+    voca_list = []
+    for i in range(len(set)):
+        words = split2word(set[i])
+        voca_list += words
+    return list(set(voca_list))
+
+def data2vector():
+    voca_list = build_list()
+    print("向量长度为{}".format(len(voca_list)))
+    dataset = pd.read_csv('data_proc.txt')
+    m, n = dataset.shape
+    # X = [0]*len(voca_list)
+    feature = ['item_name', 'user_name', 'rented_for', 'usually_wear', 'size', 'age', 'height', 'bust_size', 'weight', 'body_type', 'rating', 'price']
+    X = np.zeros((m, len(voca_list)))
+    for i in tqdm(range(m)):
+        x = dataset.iloc[i]
+        for j in feature:
+            X[i, voca_list.index(x[j])] = 1
+
+    np.save('data2vector.npy', X)
+
+def getnum_eachkind():
+    data = pd.read_csv('data_proc.txt')
+    num_t2s = 0
+    num_s = 0
+    num_l = 0
+    n, m = data.shape
+    for i in range(n):
+        if data.iat[i,2] == "True to Size":
+            num_t2s += 1
+        if data.iat[i,2] == "Small":
+            num_s += 1
+        if data.iat[i,2] == "Large":
+            num_l += 1
+    print("len of t2s is {}".format(num_t2s))
+    print("len of s is {}".format(num_s))
+    print("len of l is {}".format(num_l))
+
+def drop_nan(spath, dpath):
+    # 将训练数据集中有空信息的数据扔掉，剩余数据放入一个新文件
+    data = pd.read_csv(spath, usecols = ['item_name','price','fit','rented_for','usually_wear','size','age','height','bust_size','weight','body_type'])
+    #data = pd.read_csv(spath)
+    df = data.dropna()
+    m, n = df.shape
+    for i in range(m):
+        for j in range(n):
+            x = df.iat[i,j]
+            df.iat[i,j] = str(x).replace('\n',' ')
+    df.to_csv(dpath, index=False)
+
+
+
+
 spath = 'data_raw.txt'
 dpath = 'data_proc.txt'
 proc()
-#data = pd.read_csv(dpath)
+data = pd.read_csv(dpath)
 #print(len(data))
-
+getnum_eachkind()
+#全数据下
+# len of t2s is 16126
+# len of s is 2594
+# len of l is 3351
+#test数据下
+# len of t2s is 16303
+# len of s is 2660
+# len of l is 3413
 # print(val_range(dpath, 'fit'))
 # data = pd.read_csv(dpath)[['item_name','height','weight','rating','fit']]
 # print(data.size)
--- cyb/test_cyb.py
+++ cyb/test_cyb.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from collections import Counter
 import math
+import numpy as np
 #统计数据库数据分布情况
 # mc_df = pd.read_json(r'C:\Users\86189\PycharmProjects\MLproject\Machine-Learning\train_data_all.json')
 # #print(mc_df.head())
@@ -12,41 +13,61 @@ import math
 ############################################
    #贝叶斯回归
 
+def delete_len1(list):
+   for i in range(len(list) - 1, -1, -1):
+      if len(list[i]) == 1 and not list[i].isdigit():
+         del list[i]
+def split2word(words):
+   word = []
+   for i in range(len(words)):
+      word += words[i].split()
+   delete_len1(word)
+   return word
 
-set = []
-f = open(r"C:/Users/86189/PycharmProjects/MLproject/Machine-Learning/cyb/data_proc.txt",'r',encoding="utf-8")
-set = f.readlines()
-del(set [0])
-f.close()
-data = pd.read_csv('data_proc.txt')
 # for i in range(len(set)-1,-1,-1):#倒叙删除
 #    if set[i].startswith('"'):
 #       set[i] += set[i+1]
 #       del(set[i+1])
-
-#分出训练集和测试集,一个元素是一个代表一个sample的字符串
-# test_set = set[:3000]
-# train_set = set[3000:]
-#得到各1000的测试集
-testnum1 = 0
-testnum2 = 0
-testnum3 = 0
-test_keyset = []
-train_keyset = []
-for i in range(len(set)):
-   #words = train_set[i].split(',')#按逗号拆每行
-   line= set[i]
-   if data.iat[i,2] == "True to Size" and testnum2 < 1000:
-      test_keyset.append(i)
-      testnum2 += 1
-   if data.iat[i,2] == "Small" and testnum1 < 1000:
-      test_keyset.append(i)
-      testnum1 += 1
-   if data.iat[i,2] == "Large" and testnum3 < 1000:
-      test_keyset.append(i)
-      testnum3 += 1
-   else:
-      train_keyset.append(i)
+def set_divide(set,data):
+   #分出训练集和测试集,一个元素是一个代表一个sample的字符串
+   # test_set = set[:3000]
+   # train_set = set[3000:]
+   #得到测试集和训练集
+   testnum1 = 0
+   testnum2 = 0
+   testnum3 = 0
+   test_keyset = []
+   temp_keyset = []
+   temp1_keyset = []
+   train_keyset = []
+   for i in range(len(set)):
+      #words = train_set[i].split(',')#按逗号拆每行
+      if data.iat[i,2] == "True to Size":
+         if testnum2 < 300:
+            test_keyset.append(i)
+         else:
+            temp_keyset.append(i)
+         testnum2 += 1
+      if data.iat[i,2] == "Small":
+         if testnum1 < 300:
+            test_keyset.append(i)
+         else:
+            temp1_keyset.append(i)
+         testnum1 += 1
+      if data.iat[i,2] == "Large":
+         if testnum3 < 300:
+            test_keyset.append(i)
+         else:
+            temp1_keyset.append(i)
+         testnum3 += 1
+   temp_keyset = np.array(temp_keyset)
+   train_keyset = np.array_split(temp_keyset,5)
+   a = np.array(temp1_keyset)
+   for i in range(5):
+      train_keyset[i] = np.append(train_keyset[i],a)
+   return test_keyset,train_keyset
+   # else:
+   #    train_keyset.append(i)
    # if line.__contains__("True to Size") and testnum2 < 1000:
    #    test_set.append(line)
    #    testnum2 += 1
@@ -60,129 +81,149 @@ for i in range(len(set)):
    #    train_set.append(line)
 # print(set[1])
 # print(test_set[1])
-#print(len(test_keyset))
+# print("len of testset is {}".format(len(test_keyset)))
+# print("shape of trainset is {}".format(len(train_keyset)))
+def train(set,train_keyset):
+   #将训练集划分到三个分类中
+   T2S_set = []#True to Size
+   S_set = []#Small
+   L_set = []#Large
+   T2S_num = 0
+   S_num = 0
+   L_num = 0
+   #print(train_keyset)
+   for i in train_keyset:
+      words = set[i].split(',')#按逗号拆每行
+      if data.iat[i,2] == "True to Size":
+         word = split2word(words)
+         T2S_set += word
+         T2S_num += 1
+      if data.iat[i,2] == "Small":
+         word = split2word(words)
+         S_set += word
+         S_num += 1
+      if data.iat[i,2] == "Large":
+         word = split2word(words)
+         L_set += word
+         L_num += 1
+   #print(T2S_num,S_num,L_num)
+   #训练过程
+   V_set = list(dict.fromkeys(T2S_set + S_set + L_set))
+   V = len(V_set)
+   D = len(train_keyset)
+   #true to size
+   P_t2s = T2S_num/D
+   n_t2s = len(T2S_set)
+   n_t2s_dict = Counter(T2S_set)
+   P_k_t2s = {}
+   for word , value in n_t2s_dict.items():
+      value = (value + 1)/(n_t2s + V)
+      P_k_t2s[word] = value
+   #small
+   P_s = S_num/D
+   n_s = len(S_set)
+   n_s_dict = Counter(S_set)
+   P_k_s = {}
+   for word , value in n_s_dict.items():
+      value = (value + 1)/(n_s + V)
+      P_k_s[word] = value
+   #large
+   P_l = L_num/D
+   n_l = len(L_set)
+   n_l_dict = Counter(L_set)
+   P_k_l = {}
+   for word , value in n_l_dict.items():
+      value = (value + 1)/(n_l + V)
+      P_k_l[word] = value
+   P_list = [P_s,P_t2s,P_l]
+   P_k_list = []
+   P_k_list.append(P_k_s)
+   P_k_list.append(P_k_t2s)
+   P_k_list.append(P_k_l)
+   return P_list, P_k_list
 
+def Predict(word_list, P_list, P_k_list):#之后在每个if分支中插入Predict判断结果
+   predict_s = 0
+   predict_t2s = 0
+   predict_l = 0
+   for j in range(5):
+      y_t2s = math.log10(P_list[j][1])
+      y_s = math.log10(P_list[j][0])
+      y_l = math.log10(P_list[j][2])
+      #添加直接比较五个预测器数量
+      for i in range(len(word_list)):
+         # if spam_train.__contains__(word_list[i]):
+         temp1 = P_k_list[j][0].get(word_list[i], 1)
+         temp2 = P_k_list[j][1].get(word_list[i], 1)
+         temp3 = P_k_list[j][2].get(word_list[i], 1)
+         if temp1 == 1 or temp2 == 1 or temp3 ==1: continue
+         y_s += math.log10(temp1)
+         y_t2s += math.log10(temp2)
+         y_l += math.log10(temp3)
+         if y_s >= y_t2s:
+            predict_s += 1
+         elif y_t2s >= y_l:
+            predict_t2s += 1
+         else:
+            predict_l += 1
+   predict = 0
+   if predict_s >= predict_t2s:
+      predict = 1
+   elif predict_t2s >= predict_l:
+      predict = 2
+   else:
+      predict = 3
+   return predict-1
 
-def delete_len1(list):
-   for i in range(len(list) - 1, -1, -1):
-      if len(list[i]) == 1 and not list[i].isdigit():
-         del list[i]
-def split2word(words):
-   word = []
-   for i in range(len(words)):
-      word += words[i].split()
-   delete_len1(word)
-   return word
+def test(set, test_keyset):
+   #test
+   confusion_matrix = [[0,0,0], [0,0,0], [0,0,0]]#预测值，真实值，第二元素是预测为2，真实为1
+   for i in test_keyset:
+      #words = train_set[i].split(',')#按逗号拆每行
+      line= set[i]
+      both = 0
+      if data.iat[i,2] == "True to Size":
+         line = line.split(',')
+         line.remove("True to Size")
+         word_list = split2word(line)
+         confusion_matrix[2-1][Predict(word_list, P_list, P_k_list)] += 1
+         both += 1
+      if data.iat[i,2] == "Small":
+         line = line.split(',')
+         line.remove("Small")
+         word_list = split2word(line)
+         confusion_matrix[1-1][Predict(word_list, P_list, P_k_list)] += 1
+         both += 1
+      if data.iat[i,2] == "Large":
+         line = line.split(',')
+         line.remove("Large")
+         word_list = split2word(line)
+         confusion_matrix[3-1][Predict(word_list, P_list, P_k_list)] += 1
+         both += 1
+      # if data.iat[i, 5] == "Large":
+      #    line = line.replace('Large', '')
+      #    word_list = split2word(line.split(','))
+      #    confusion_matrix[3-1][Predict(word_list, 3, y_t2s, y_s, y_l)] += 1
+      #    both += 1
+      # if both > 1:
+      #    print(set[i])
+   return confusion_matrix
 
-#将训练集划分到三个分类中
-T2S_set = []#True to Size
-S_set = []#Small
-L_set = []#Large
-T2S_num = 0
-S_num = 0
-L_num = 0
-for i in train_keyset:
-   words = set[i].split(',')#按逗号拆每行
-   if data.iat[i,2] == "True to Size":
-      word = split2word(words)
-      T2S_set += word
-      T2S_num += 1
-   if data.iat[i,2] == "Small":
-      word = split2word(words)
-      S_set += word
-      S_num += 1
-   if data.iat[i,2] == "Large":
-      word = split2word(words)
-      L_set += word
-      L_num += 1
-
-
-#训练过程
-V_set = list(dict.fromkeys(T2S_set + S_set + L_set))
-V = len(V_set)
-D = len(train_keyset)
-#true to size
-P_t2s = T2S_num/D
-n_t2s = len(T2S_set)
-n_t2s_dict = Counter(T2S_set)
-P_k_t2s = {}
-for word , value in n_t2s_dict.items():
-   value = (value + 1)/(n_t2s + V)
-   P_k_t2s[word] = value
-#small
-P_s = S_num/D
-n_s = len(S_set)
-n_s_dict = Counter(S_set)
-P_k_s = {}
-for word , value in n_s_dict.items():
-   value = (value + 1)/(n_s + V)
-   P_k_s[word] = value
-#large
-P_l = L_num/D
-n_l = len(L_set)
-n_l_dict = Counter(L_set)
-P_k_l = {}
-for word , value in n_l_dict.items():
-   value = (value + 1)/(n_l + V)
-   P_k_l[word] = value
-
-#test
-predict = 0 # 1 =small. 2 = true to size. 3 = large
-confusion_matrix = [[0,0,0], [0,0,0], [0,0,0]]#预测值，真实值，第二元素是预测为2，真实为1
-accuracy = 0
-y_t2s = math.log10(P_t2s)
-y_s = math.log10(P_s)
-y_l = math.log10(P_l)
-def Predict(word_list, label, y_t2s, y_s, y_l):#之后在每个if分支中插入Predict判断结果
-   for i in range(len(word_list)):
-      # if spam_train.__contains__(word_list[i]):
-      temp1 = P_k_s.get(word_list[i], 1)
-      temp2 = P_k_t2s.get(word_list[i], 1)
-      temp3 = P_k_l.get(word_list[i], 1)
-      if temp1 == 1 or temp2 == 1 or temp3 ==1: continue
-      y_s += math.log10(temp1)
-      y_t2s += math.log10(temp2)
-      y_l += math.log10(temp3)
-      if y_s >= y_t2s:
-         predict = 1
-      elif y_t2s >= y_l:
-         predict = 2
-      else:
-         predict = 3
-      return predict-1
-
-
-
-for i in test_keyset:
-   #words = train_set[i].split(',')#按逗号拆每行
-   line= set[i]
-   both = 0
-   if data.iat[i,2] == "True to Size":
-      line = line.split(',')
-      line.remove("True to Size")
-      word_list = split2word(line)
-      confusion_matrix[2-1][Predict(word_list, 2, y_t2s, y_s, y_l)] += 1
-      both += 1
-   if data.iat[i,2] == "Small":
-      line = line.split(',')
-      line.remove("Small")
-      word_list = split2word(line)
-      confusion_matrix[1-1][Predict(word_list, 1, y_t2s, y_s, y_l)] += 1
-      both += 1
-   if data.iat[i,2] == "Large":
-      line = line.split(',')
-      line.remove("Large")
-      word_list = split2word(line)
-      confusion_matrix[3-1][Predict(word_list, 3, y_t2s, y_s, y_l)] += 1
-      both += 1
-   # if data.iat[i, 5] == "Large":
-   #    line = line.replace('Large', '')
-   #    word_list = split2word(line.split(','))
-   #    confusion_matrix[3-1][Predict(word_list, 3, y_t2s, y_s, y_l)] += 1
-   #    both += 1
-   if both > 1:
-      print(set[i])
-
+set = []
+f = open(r"C:/Users/86189/PycharmProjects/MLproject/Machine-Learning/cyb/data_proc.txt",'r',encoding="utf-8")
+set = f.readlines()
+del(set [0])
+f.close()
+data = pd.read_csv('data_proc.txt')
+test_keyset, train_keyset = set_divide(set, data)
+P_list = []
+P_k_list = []
+for i in range(5):
+   x, y = train(set,train_keyset[i])
+   P_list.append(x)
+   P_k_list.append(y)
+#print(P_list)
+confusion_matrix = test(set,test_keyset)
 accuracy_s = confusion_matrix[0][0]/(confusion_matrix[0][0]+confusion_matrix[1][0]+confusion_matrix[2][0])
 recall_s = confusion_matrix[0][0]/(confusion_matrix[0][0]+confusion_matrix[0][1]+confusion_matrix[0][2])
 accuracy_t2s = confusion_matrix[1][1]/(confusion_matrix[0][1]+confusion_matrix[1][1]+confusion_matrix[2][1])
@@ -227,3 +268,25 @@ print("recall_l is {}".format(recall_l))
 # recall_t2s is 0.99
 # accuracy_l is 0.75
 # recall_l is 0.018
+
+#5 predictor contain all attributes
+# predict_s is 497
+# predict_t2s is 292
+# predict_l is 111
+# accuracy_s is 0.5070422535211268
+# recall_s is 0.84
+# accuracy_t2s is 0.6883561643835616
+# recall_t2s is 0.67
+# accuracy_l is 0.4954954954954955
+# recall_l is 0.18333333333333332
+
+# 5predictors test attributes
+# predict_s is 708
+# predict_t2s is 115
+# predict_l is 77
+# accuracy_s is 0.3432203389830508
+# recall_s is 0.81
+# accuracy_t2s is 0.3826086956521739
+# recall_t2s is 0.14666666666666667
+# accuracy_l is 0.35064935064935066
+# recall_l is 0.09
